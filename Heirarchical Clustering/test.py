b82a20c079e12eced6bea35f605fd111abb51f87
Saketa Chandra Chalamchala <saketachandra.chalamchala@stonybrook.edu>
2015-12-15 11:50:06 -0500

    for n clusters - working code

---
+++ Heirarchical Clustering/test.py
@@ -0,0 +1,21 @@
+import re
+import networkx as nx
+import numpy as np
+
+#line = '[1, 10, 0,]'
+#print line
+#line = list(re.sub("\[|,|\]", "", line).split( ))
+#line = [int(x) for x in line]
+#print line
+
+
+list = [[0,1,1,0],[0,0,1,0],[1,0,1,0],[1,0,0,0]]
+l = np.zeros(shape=(3,2))
+print l
+print int(l[0][0])
+#list = np.array(list)
+#G = nx.to_networkx_graph(list, None, False)
+#print G.nodes(False)
+#print G.edges(None, False, None)
+
+#print len(list[0])
